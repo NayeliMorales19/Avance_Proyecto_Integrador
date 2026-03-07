5fb1d407af42ff82033fbae406783e6673340bd0
NayeliMorales19 <moralesnayeli120217@gmail.com>
2026-03-07 17:51:55 -0600

    se agregaron las rutas para las paginas.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+import { Menu } from './pages/menu/menu';
+
+export const routes: Routes = [
+
+  { path: '', component: Login },
+  { path: 'register', component: Register },
+  { path: 'menu', component: Menu }
+
+];
