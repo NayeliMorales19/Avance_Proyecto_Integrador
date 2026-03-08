9ea3c2824316431ffcb4d45f1bd30669e3386962
NayeliMorales19 <moralesnayeli120217@gmail.com>
2026-03-07 18:39:15 -0600

    se agrego funcionamiento

--- src/app/guards/auth-guard.ts
+++ src/app/guards/auth-guard.ts
@@ -1,5 +1,17 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+export const authGuard: CanActivateFn = () => {
+
+  const router = inject(Router);
+
+  const user = localStorage.getItem("loggedUser");
+
+  if(user){
+    return true;
+  }
+
+  router.navigate(['/']);
+  return false;
+
 };
