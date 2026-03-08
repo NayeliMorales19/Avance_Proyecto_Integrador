521cdd0bd78662d52715c2bd9afd48ce9db61b74
NayeliMorales19 <moralesnayeli120217@gmail.com>
2026-03-07 18:27:16 -0600

    conexion de login por autenticacion

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [FormsModule,RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+
+  email=''
+  password=''
+
+  constructor(private auth:AuthService, private router:Router){}
+
+  login(){
+
+    const user=this.auth.login(this.email,this.password);
+
+    if(user){
+
+      this.router.navigate(['/menu']);
+
+    }else{
+
+      alert("Usuario no registrado");
+
+    }
+
+  }
 
 }
