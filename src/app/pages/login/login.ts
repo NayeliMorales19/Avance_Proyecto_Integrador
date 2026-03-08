4867d580fb773f6e2d737f0663531e4d25a1b4e4
NayeliMorales19 <moralesnayeli120217@gmail.com>
2026-03-07 18:02:23 -0600

    se agrego la importacion RouteLink

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrl: './login.css'
 })
-export class Login {}
+export class Login {
+
+}
