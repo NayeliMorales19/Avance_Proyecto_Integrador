ab4eb1bdf4cdd3c5e9954577ad269433b5131b6f
NayeliMorales19 <moralesnayeli120217@gmail.com>
2026-03-07 18:43:52 -0600

    actualizacion de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,19 @@ import { Routes } from '@angular/router';
 
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
-import { Menu } from './pages/menu/menu';
+import { Menu  } from './pages/menu/menu';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
 
-  { path: '', component: Login },
-  { path: 'register', component: Register },
-  { path: 'menu', component: Menu }
+{ path: '', component: Login },
+
+{ path: 'register', component: Register },
+
+{ 
+  path: 'menu',
+  component: Menu,
+  canActivate: [authGuard]
+}
 
 ];
