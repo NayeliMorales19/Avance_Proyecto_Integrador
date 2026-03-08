8dbf4d6a00d41dbc0d7410fe62f3641b7215e316
NayeliMorales19 <moralesnayeli120217@gmail.com>
2026-03-07 18:25:07 -0600

    se agrego el codigo para que funcione la autenticacion

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -1,6 +1,28 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Auth {}
+export class AuthService {
+
+  register(user:any){
+
+    const users = JSON.parse(localStorage.getItem('users') || '[]');
+
+    users.push(user);
+
+    localStorage.setItem('users', JSON.stringify(users));
+
+  }
+
+  login(email:string,password:string){
+
+    const users = JSON.parse(localStorage.getItem('users') || '[]');
+
+    return users.find((u:any)=>
+      u.email === email && u.password === password
+    );
+
+  }
+
+}
